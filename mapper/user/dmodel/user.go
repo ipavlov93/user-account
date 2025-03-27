6c70357233d07c79af8192efc812d819a0009db8
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-03-27 12:02:15 +0200

    add user mapper dto

--- mapper/user/dmodel/user.go
+++ mapper/user/dmodel/user.go
@@ -3,6 +3,7 @@ package dmodel
 import (
 	"event-calendar/dmodel"
 	"event-calendar/internal/domain"
+	"event-calendar/smodel"
 )
 
 func UserToUserDto(user domain.User) dmodel.User {
@@ -24,6 +25,19 @@ func UserDtoToUser(user dmodel.User) domain.User {
 		FirstName:    user.FirstName,
 		LastName:     user.LastName,
 		EmailAddress: user.EmailAddress,
+		Company:      user.Company,
+		Description:  user.Description,
+	}
+}
+
+// MapDto maps Smodel.User to Dmodel.User
+func MapDto(user smodel.User) dmodel.User {
+	return dmodel.User{
+		ID:           user.ID,
+		UUID:         user.UUID,
+		FirstName:    user.FirstName,
+		LastName:     user.LastName,
+		EmailAddress: user.EmailAddress.Address,
 		Company:      user.Company,
 		Description:  user.Description,
 	}
--- mapper/user/smodel/user.go
+++ mapper/user/smodel/user.go
@@ -1,6 +1,7 @@
 package smodel
 
 import (
+	"event-calendar/dmodel"
 	"event-calendar/internal/domain"
 	"event-calendar/smodel"
 	"fmt"
@@ -31,5 +32,21 @@ func UserDtoToUser(user smodel.User) domain.User {
 		EmailAddress: user.EmailAddress.Address,
 		Company:      user.Company,
 		Description:  user.Description,
+	}
+}
+
+// MapDto maps Dmodel.User to Smodel.User
+func MapDto(user dmodel.User) smodel.User {
+	return smodel.User{
+		ID:        user.ID,
+		UUID:      user.UUID,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		EmailAddress: mail.Address{
+			Name:    fmt.Sprintf("%s %s", user.FirstName, user.LastName),
+			Address: user.EmailAddress,
+		},
+		Company:     user.Company,
+		Description: user.Description,
 	}
 }
