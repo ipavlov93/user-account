70cfede8e45b60e61ca4e571940a550861b2d380
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-14 16:28:40 +0300

    add database package with DBAdapter and PostgresAdapter

---
+++ internal/database/adapter.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"github.com/jmoiron/sqlx"
+)
+
+type DBAdapter interface {
+	GetConnection() *sqlx.DB
+	MustBeginTx(ctx context.Context, options *sql.TxOptions) *sqlx.Tx
+	// MustRollbackTxUnlessCommitted name is preferred than MustCommit
+	MustRollbackTxUnlessCommitted(tx *sqlx.Tx) error
+}
---
+++ internal/database/postgres.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" //The postgres driver
+)
+
+type PostgresAdapter struct {
+	sqlxDB *sqlx.DB
+}
+
+func NewPostgresAdapter(host string, port int, user, password, dbname string) PostgresAdapter {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	connection, err := sqlx.Connect("postgres", psqlInfo)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database, %s", err))
+	}
+
+	return PostgresAdapter{sqlxDB: connection}
+}
+
+func (db *PostgresAdapter) GetConnection() *sqlx.DB {
+	return db.sqlxDB
+}
+
+// MustBeginTx tries to begin transaction, panics if begin has failed.
+func (db *PostgresAdapter) MustBeginTx(ctx context.Context, options *sql.TxOptions) *sqlx.Tx {
+	tx, err := db.sqlxDB.BeginTxx(ctx, options)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start transaction, %s", err))
+	}
+	return tx
+}
+
+// mustRollbackTx tries to rollback transaction, panics if rollback failed.
+func mustRollbackTx(tx *sqlx.Tx) {
+	err := tx.Rollback()
+	if err != nil {
+		panic(fmt.Sprintf("Failed to rollback transaction, %s", err))
+	}
+}
+
+// MustRollbackTxUnlessCommitted commits the not nil transaction.
+// It tries to rollback tx if commit has failed.
+// MustRollbackTxUnlessCommitted name is preferred than MustCommit.
+func (db *PostgresAdapter) MustRollbackTxUnlessCommitted(tx *sqlx.Tx) error {
+	if tx == nil {
+		return fmt.Errorf("tx is empty")
+	}
+	if err := tx.Commit(); err != nil {
+		mustRollbackTx(tx)
+		return err
+	}
+
+	return nil
+}
