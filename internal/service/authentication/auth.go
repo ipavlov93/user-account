fc318bd47b774ab3ebc2176662383e8f13d9036f
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-14 16:39:55 +0300

    rename AuthService to FirebaseAuthService

--- internal/service/authentication/auth.go
+++ internal/service/authentication/auth.go
@@ -1,4 +1,4 @@
-package auth
+package authentication
 
 import (
 	"context"
@@ -9,11 +9,11 @@ import (
 	"firebase.google.com/go/v4/auth"
 )
 
-type AuthService struct {
+type FirebaseAuthService struct {
 	firebaseAuthService *auth.Client
 }
 
-func NewAuthService(firebaseCredentialFilePath string) AuthService {
+func NewAuthService(firebaseCredentialFilePath string) FirebaseAuthService {
 	opt := option.WithCredentialsFile(firebaseCredentialFilePath)
 
 	ctx := context.Background()
@@ -31,7 +31,7 @@ func NewAuthService(firebaseCredentialFilePath string) AuthService {
 		panic("firebase auth service init failed")
 	}
 
-	return AuthService{
+	return FirebaseAuthService{
 		firebaseAuthService: firebaseAuthService,
 	}
 }
@@ -39,7 +39,7 @@ func NewAuthService(firebaseCredentialFilePath string) AuthService {
 // VerifyIDToken uses Firebase SDK (to implement OIDC flow) to verify ID token.
 // Returns an err if token is invalid, expired, disabled or revoked.
 // TODO: Add key to cache
-func (s AuthService) VerifyIDToken(idToken string) (token *auth.Token, err error) {
+func (s FirebaseAuthService) VerifyIDToken(idToken string) (token *auth.Token, err error) {
 	//if idToken == "" {
 	//	return token, ErrInvalidToken
 	//}
@@ -64,13 +64,15 @@ func (s AuthService) VerifyIDToken(idToken string) (token *auth.Token, err error
 	//	return token, err
 	//}
 
-	// TODO: Add key to cache
+	// TODO:
+	// 1. Add key to (session) cache if SetFirebaseUID doesn't work
+	// 1. test auth after SetFirebaseUID, does token (access or id?) contains firebaseUID
 
 	return token, nil
 }
 
 // TODO: remove token from cache
-func (s AuthService) RevokeRefreshTokens(ctx context.Context, idToken string) error {
+func (s FirebaseAuthService) RevokeRefreshTokens(ctx context.Context, idToken string) error {
 	token, err := s.firebaseAuthService.VerifyIDToken(ctx, idToken)
 	if err != nil {
 		return err
@@ -84,4 +86,13 @@ func (s AuthService) RevokeRefreshTokens(ctx context.Context, idToken string) er
 	// TODO: remove auth tokens from cache
 
 	return nil
+}
+
+// SetFirebaseUID sets Firebase UID as a custom claim to token (id or access token)?
+func (s FirebaseAuthService) SetFirebaseUID(firebaseUID string) error {
+	claims := map[string]interface{}{
+		"firebase_uid": firebaseUID,
+	}
+
+	return s.firebaseAuthService.SetCustomUserClaims(context.Background(), firebaseUID, claims)
 }
