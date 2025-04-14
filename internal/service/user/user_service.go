a612194f47205179fddea6fd04fd7b769390c4e0
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-14 16:38:33 +0300

    update user service. add DI using constructor.

--- internal/service/user/user_service.go
+++ internal/service/user/user_service.go
@@ -4,20 +4,25 @@ import (
 	"context"
 
 	"event-calendar/internal/domain"
+	repository "event-calendar/internal/repository/postgres"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type UserService struct {
-	repository UserRepository
+	dbDriver sqlx.ExtContext
 }
 
-func NewUserService(repository UserRepository) *UserService {
+func NewUserService(dbDriver sqlx.ExtContext) *UserService {
 	return &UserService{
-		repository,
+		dbDriver: dbDriver,
 	}
 }
 
 func (s UserService) GetUserByID(ctx context.Context, id int64) (user domain.User, found bool, err error) {
-	user, err = s.repository.GetUserByID(ctx, id)
+	repo := repository.NewUserRepository(s.dbDriver)
+
+	user, err = repo.GetUserByID(ctx, id)
 	if err != nil {
 		//if errors.Is(err, repository.ErrNoRows) {
 		//return customError with status code NotFound
@@ -29,7 +34,9 @@ func (s UserService) GetUserByID(ctx context.Context, id int64) (user domain.Use
 }
 
 func (s UserService) GetUserByUUID(ctx context.Context, uuid string) (user domain.User, found bool, err error) {
-	user, err = s.repository.GetUserByUUID(ctx, uuid)
+	repo := repository.NewUserRepository(s.dbDriver)
+
+	user, err = repo.GetUserByFirebaseUID(ctx, uuid)
 	if err != nil {
 		//if errors.Is(err, repository.ErrNoRows) {
 		//return customError with status code NotFound
@@ -41,7 +48,9 @@ func (s UserService) GetUserByUUID(ctx context.Context, uuid string) (user domai
 }
 
 func (s UserService) CreateUser(ctx context.Context, user domain.User) (int64, error) {
-	userID, err := s.repository.CreateUser(ctx, user)
+	repo := repository.NewUserRepository(s.dbDriver)
+
+	userID, err := repo.CreateUser(ctx, user)
 	if err != nil {
 		//if errors.Is(err, repository.ErrDuplicate) {
 		//return customError with status code BadRequest
