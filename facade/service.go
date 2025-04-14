384cc6a8eda3f79dd984223c6e64ac67e675339b
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-14 16:40:27 +0300

    update service contracts

--- facade/service.go
+++ facade/service.go
@@ -2,18 +2,33 @@ package facade
 
 import (
 	"context"
+	"database/sql"
 
 	"event-calendar/internal/domain"
+	"event-calendar/internal/domain/claims"
+
+	"firebase.google.com/go/v4/auth"
 )
 
-type UserService interface {
-	GetUserByID(ctx context.Context, id int64) (user domain.User, found bool, err error)
-	GetUserByUUID(ctx context.Context, uuid string) (user domain.User, found bool, err error)
-	CreateUser(ctx context.Context, user domain.User) (int64, error)
+type AuthService interface {
+	SignUp(ctx context.Context, claims *claims.FirebaseAuthClaims) error
+	Login(ctx context.Context, claims *claims.FirebaseAuthClaims) error
+	Logout(ctx context.Context, token string) error
 }
 
-type AuthService interface {
-	SignUp(ctx context.Context, token string) error
-	Login(ctx context.Context, token string) error
-	Logout(ctx context.Context, token string) error
+type FirebaseAuthService interface {
+	VerifyIDToken(idToken string) (token *auth.Token, err error)
+	RevokeRefreshTokens(ctx context.Context, idToken string) error
+	SetFirebaseUID(firebaseUID string) error
+}
+
+type UserService interface {
+	CreateUser(ctx context.Context, user domain.User, tx ...*sql.Tx) (int64, error)
+	GetUserByID(ctx context.Context, id int64) (user domain.User, found bool, err error)
+	GetUserByUUID(ctx context.Context, uuid string) (user domain.User, found bool, err error)
+}
+
+type UserAccountService interface {
+	CreateUserAccount(ctx context.Context, userAccount domain.UserAccount, ignoreDuplicate bool, tx ...*sql.Tx) (int64, error)
+	ListUserAccountsByUserID(ctx context.Context, userID int64) (userAccountsList []domain.UserAccount, err error)
 }
