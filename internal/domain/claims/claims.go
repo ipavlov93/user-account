34b9807884caf5c48d7a60c0c582bc5d50a1cb9e
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-16 13:47:31 +0300

    edit UserClaims FirebaseUID

--- internal/domain/claims/claims.go
+++ internal/domain/claims/claims.go
@@ -2,10 +2,10 @@ package claims
 
 // UserClaims represents extracted claims from an access token
 type UserClaims struct {
-	Subject string `json:"sub"` // User ID
-	Issuer  string `json:"iss"` // Auth provider (Google, Azure)
-	Email   string `json:"email"`
+	FirebaseUID string `json:"user_id"`
+	Subject     string `json:"sub"` // Auth provider User ID
+	Issuer      string `json:"iss"` // Auth provider (Google, Azure)
+	Email       string `json:"email"`
 
-	FirebaseUID string `json:"firebase_uid"`
-	Roles       []Role `json:"roles"`
+	Roles []Role `json:"roles"`
 }
