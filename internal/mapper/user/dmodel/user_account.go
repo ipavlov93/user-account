3334aea7340b8b03eb1ceef5e5bbe2fcdeb42a47
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-17 12:35:39 +0300

    add ContactName to UserAccount mapper.

--- internal/mapper/user/dmodel/user_account.go
+++ internal/mapper/user/dmodel/user_account.go
@@ -1,10 +1,13 @@
 package dmodel
 
 import (
+	"database/sql"
 	"event-calendar/internal/domain"
 	"event-calendar/internal/dto/dmodel"
 )
 
+// UserAccountToDto
+// IMPORTANT: ignore field CreatedAt.
 func UserAccountToDto(userAccount domain.UserAccount) dmodel.UserAccount {
 	return dmodel.UserAccount{
 		ID:           userAccount.ID,
@@ -12,6 +15,10 @@ func UserAccountToDto(userAccount domain.UserAccount) dmodel.UserAccount {
 		IssuerCode:   userAccount.IssuerCode,
 		SubjectUID:   userAccount.SubjectUID,
 		EmailAddress: userAccount.EmailAddress,
+		ContactName: sql.NullString{
+			Valid:  len(userAccount.ContactName) > 0,
+			String: userAccount.ContactName,
+		},
 	}
 }
 
@@ -22,6 +29,7 @@ func DtoToUserAccount(userAccount dmodel.UserAccount) domain.UserAccount {
 		IssuerCode:   userAccount.IssuerCode,
 		SubjectUID:   userAccount.SubjectUID,
 		EmailAddress: userAccount.EmailAddress,
+		ContactName:  userAccount.ContactName.String,
 	}
 }
 
@@ -34,6 +42,7 @@ func DtosToUserAccounts(userAccounts []dmodel.UserAccount) []domain.UserAccount 
 			IssuerCode:   userAccount.IssuerCode,
 			SubjectUID:   userAccount.SubjectUID,
 			EmailAddress: userAccount.EmailAddress,
+			ContactName:  userAccount.ContactName.String,
 		})
 	}
 	return result
