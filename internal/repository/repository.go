1bd56de19082717a8cc4b75c2c599b6fb86c1e6d
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-18 08:58:54 +0300

    forced to move Repository contracts to repository package

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,11 +1,19 @@
-package userservice
+package repository
 
 import (
 	"context"
+
 	"event-calendar/internal/domain"
+
+	"github.com/jmoiron/sqlx"
 )
 
+type WithTx[T any] interface {
+	WithTx(tx *sqlx.Tx) T
+}
+
 type UserRepository interface {
+	WithTx[UserRepository]
 	CreateUser(ctx context.Context, user domain.User) (int64, error)
 	GetUsersCount(ctx context.Context) (int64, error)
 	GetUserByID(ctx context.Context, id int64) (domain.User, error)
@@ -13,6 +21,7 @@ type UserRepository interface {
 }
 
 type UserProfileRepository interface {
+	WithTx[UserProfileRepository]
 	CreateUserProfile(ctx context.Context, user domain.UserProfile) (int64, error)
 	GetUserProfilesCount(ctx context.Context) (int64, error)
 	GetUserProfileByID(ctx context.Context, id int64) (user domain.UserProfile, err error)
@@ -21,6 +30,7 @@ type UserProfileRepository interface {
 }
 
 type UserAccountRepository interface {
+	WithTx[UserAccountRepository]
 	CreateUserAccount(ctx context.Context, user domain.UserAccount, ignoreDuplicate bool) (userAccountID int64, err error)
 	ListUserAccountsByUserID(ctx context.Context, userID int64) (userAccounts []domain.UserAccount, err error)
 }
