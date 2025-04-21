ab7fc5014e5c05e87e038668de96bb7af5be49b0
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-21 13:02:47 +0300

    add domain.User Go Doc comments

---
+++ internal/domain/docs.go
@@ -0,0 +1,3 @@
+// Package domain defines the core business entities and logic for the Bookly application.
+// It encapsulates the primary data models and their associated behaviors.
+package domain
