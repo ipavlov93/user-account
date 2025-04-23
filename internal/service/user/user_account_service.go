9445fb7a07f777c31737503493bb1d7c037db4e5
Ivan Pavlov <ivan.pavlov@nixsolutions.com>
2025-04-23 17:49:13 +0300

    refactor UserAccountService

--- internal/service/user/user_account_service.go
+++ internal/service/user/user_account_service.go
@@ -4,8 +4,8 @@ import (
 	"context"
 
 	"event-calendar/internal/domain"
+	"event-calendar/internal/option"
 	"event-calendar/internal/repository"
-	"event-calendar/internal/service/user/option"
 )
 
 type UserAccountService struct {
@@ -24,7 +24,10 @@ func (s UserAccountService) ListUserAccountsByUserID(
 	ctx context.Context,
 	userID int64,
 	options *option.TxOption,
-) (userAccountsList []domain.UserAccount, err error) {
+) (
+	userAccountsList []domain.UserAccount,
+	err error,
+) {
 	// inject tx into repository
 	repo := option.ApplyTx(s.userAccountRepository, options)
 
@@ -33,7 +36,7 @@ func (s UserAccountService) ListUserAccountsByUserID(
 		//if errors.Is(err, repository.ErrNoRows) {
 		//return customError with status code NotFound
 		//}
-		return userAccountsList, err
+		return nil, err
 	}
 
 	return userAccountsList, nil
